20f1e3ea2a5baebf06e36edd1dd699df3e3f73bc
Paul Roberts <paul.roberts@theguardian.com>
2020-11-16 19:42:55 +0000

    when sending empty params, ensure the resulting json is an object
    instead of an array

--- init.lua
+++ init.lua
@@ -317,12 +317,16 @@ function Server:request(method, params)
   return message.result ~= json.null and message.result or nil
 end
 
+-- has the metatable attribute __jsontype = 'object' to make sure it
+-- is converted into an empty object rather than an empty array
+local empty_object = json.decode("{}")
+
 ---
 -- Sends a notification to this language server.
 -- @param method String method name of the notification.
 -- @param params Table of parameters for the notification.
 function Server:notify(method, params)
-  local message = {jsonrpc = '2.0', method = method, params = params or {}}
+  local message = {jsonrpc = '2.0', method = method, params = params or empty_object}
   local data = json.encode(message)
   if M.log_rpc then self:log('RPC send: ' .. data) end
   self.proc:write(string.format(
